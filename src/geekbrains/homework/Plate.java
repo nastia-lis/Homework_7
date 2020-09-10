3ea3f81003487244359a1d3105974cfe5689b564
nastia <nastiusha_super@mail.ru>
2020-09-10 22:20:28 +0300

    hw 7

--- src/geekbrains/homework/Plate.java
+++ src/geekbrains/homework/Plate.java
@@ -1,4 +1,34 @@
 package geekbrains.homework;
 
 public class Plate {
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void decreaseFood(int amount) {
+        this.food -= amount;
+        if (food < amount) {
+            System.out.println("Недостаточно еды!");
+        } else {
+            System.out.println("Поел!");
+        }
+    }
+
+    public void addFood(int amount) {
+        this.food += amount;
+        System.out.println("Добавили " + amount + " еды.");
+    }
+
+    @Override
+    public String toString() {
+        return "Plate{" +
+                "food=" + food +
+                '}';
+    }
 }
